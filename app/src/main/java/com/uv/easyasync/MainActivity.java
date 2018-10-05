60664f0cf1ce347678423ac986b68573e6a0a40a
hp <ymahajan1010@gmail.com>
2018-10-05 12:46:56 +0530

    test

--- app/src/main/java/com/uv/easyasync/MainActivity.java
+++ app/src/main/java/com/uv/easyasync/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This Is my test code 
+        //This Is my test code
     }
 }
