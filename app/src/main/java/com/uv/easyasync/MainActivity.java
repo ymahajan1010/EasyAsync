ebe488da2461544f663585b2fcea59fdd617154e
hp <ymahajan1010@gmail.com>
2018-10-05 12:29:49 +0530

    Initial commit

---
+++ app/src/main/java/com/uv/easyasync/MainActivity.java
@@ -0,0 +1,13 @@
+package com.uv.easyasync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
